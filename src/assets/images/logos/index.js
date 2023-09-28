eddeed65921d077bec8ae3b2a646868d46ae0d5a
Blissh <anthony.aka21@gmail.com>
2023-09-28 00:36:41 -0500

    experience section

---
+++ src/assets/images/logos/index.js
@@ -0,0 +1,8 @@
+import asecones from "./asecones-logo.png";
+import claro from "./claro-logo.png";
+import ingeant from "./ingeant-logo.png";
+import inspur from "./inspur-logo.png";
+import movistar from "./movistar-logo.png";
+import qmc from "./qmc-logo.png";
+
+export default { asecones, claro, ingeant, inspur, movistar, qmc };
