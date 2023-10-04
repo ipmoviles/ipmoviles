cdcf4f926349374bc83f3138e8317e9eb09e1ad3
Blissh <anthony.aka21@gmail.com>
2023-10-03 19:50:28 -0500

    working on i18config

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-    defaultLocale: "en",
-    locales: ["en", "es"],
-    routes: {
-      es: {
-        about: "about",
-        services: "services",
-        contact: "contact",
-        gallery: "gallery"
-      }
+  defaultLocale: "en",
+  locales: ["en", "es"],
+  routes: {
+    es: {
+      about: "about",
+      services: "services",
+      contact: "contact",
+      gallery: "gallery",
     },
-    load: ["server"]
-  };
+  },
+  preload: ["en","es"],
+  load: ["server"],
+  i18nextServer: {
+    debug: true,
+  },
+};
