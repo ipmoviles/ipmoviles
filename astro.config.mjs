690e44ae666202ea7510d74e09870759a2902fd9
Blissh <anthony.aka21@gmail.com>
2023-10-03 19:02:45 -0500

    first deployment v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,4 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [astroI18next()],
-  output: "server",
-  adapter: vercel(),
 });
