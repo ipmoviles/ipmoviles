f295aacb441273eba6f51df0e30849e7a890471e
Blissh <anthony.aka21@gmail.com>
2023-10-13 20:24:36 -0500

    navbar updated with nextui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,92 +8,141 @@ import {
   NavbarMenuItem,
   Button,
   Link,
+  Divider,
 } from "@nextui-org/react";
 import { IPLogo } from "./IPLogo";
 import { useState } from "react";
 import { DarkMode } from "./DarkMode";
+import "./styles.scss";
 
-export default function App() {
+export default function App(props) {
+  const {
+    home,
+    homeRef,
+    about,
+    aboutRef,
+    services,
+    servicesRef,
+    contact,
+    contactRef,
+    gallery,
+    galleryRef,
+    languageSelect,
+  } = props;
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
-
   return (
-    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} height="60px">
-      <NavbarContent className="sm:hidden" justify="start">
+    <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      height="60px"
+      shouldHideOnScroll
+      maxWidth="xl"
+    >
+      <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="md:hidden"
         />
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
+        <NavbarBrand className="ml-2">
           <IPLogo />
-          <p className="font-bold text-inherit ml-2">IP Moviles</p>
+          <p className="font-bold text-inherit ml-4">ipmoviles</p>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-          <IPLogo />
-          <p className="font-bold text-inherit ml-2">IP Moviles</p>
-        </NavbarBrand>
+      <NavbarContent className="hidden md:flex gap-8" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
+          <Link color="foreground" href={homeRef} className="text-2xl" isBlock>
+            {home}
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+          <Link color="foreground" href={aboutRef} className="text-2xl" isBlock>
+            {about}
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href={servicesRef}
+            className="text-2xl"
+            isBlock
+          >
+            {services}
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            color="foreground"
+            href={galleryRef}
+            className="text-2xl"
+            isBlock
+          >
+            {gallery}
           </Link>
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
         <NavbarItem>
+          <Button
+            as={Link}
+            color="secondary"
+            size="sm"
+            href={contactRef}
+            variant="flat"
+            className="text-2xl"
+          >
+            {contact}
+          </Button>
+        </NavbarItem>
+        <NavbarItem className="hidden md:flex">{languageSelect}</NavbarItem>
+        <NavbarItem className="mt-2">
           <DarkMode />
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarMenu className="sm:hidden">
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2
-                  ? "warning"
-                  : index === menuItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
+      <NavbarMenu className="md:hidden gap-8 px-10 py-8">
+        <NavbarMenuItem className="mt-4">
+          <Link color="foreground" href={homeRef} className="text-3xl w-full">
+            {home}
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link color="foreground" href={aboutRef} className="text-3xl w-full">
+            {about}
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link
+            color="foreground"
+            href={servicesRef}
+            className="text-3xl w-full"
+          >
+            {services}
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link
+            color="foreground"
+            href={galleryRef}
+            className="text-3xl w-full"
+          >
+            {gallery}
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link
+            color="foreground"
+            href={contactRef}
+            className="text-3xl w-full"
+          >
+            {contact}
+          </Link>
+        </NavbarMenuItem>
+        <Divider />
+        <NavbarItem>{languageSelect}</NavbarItem>
       </NavbarMenu>
     </Navbar>
   );
