45932a8a5b241f7876c006ab836c43533175f875
Blissh <anthony.aka21@gmail.com>
2023-10-19 19:13:54 -0500

    added metadata

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -40,22 +40,6 @@ export default function App(props) {
       height="60px"
       shouldHideOnScroll
       maxWidth="xl"
-      classNames={{
-        item: [
-          "flex",
-          "relative",
-          "h-full",
-          "items-center",
-          "data-[active=true]:after:content-['']",
-          "data-[active=true]:after:absolute",
-          "data-[active=true]:after:bottom-0",
-          "data-[active=true]:after:left-0",
-          "data-[active=true]:after:right-0",
-          "data-[active=true]:after:h-[2px]",
-          "data-[active=true]:after:rounded-[2px]",
-          "data-[active=true]:after:bg-primary",
-        ],
-      }}
     >
       <NavbarContent>
         <NavbarMenuToggle
