1a2fb5d20980cd5589f1841190894f25a0133fd2
AyaJuanD <134539283+AyaJuanD@users.noreply.github.com>
2024-06-11 11:18:16 -0500

    RestaurarGaleria

--- src/assets/images/gallery/index.js
+++ src/assets/images/gallery/index.js
@@ -1,6 +1,11 @@
 import image01 from "./1.jpg";
+import image02 from "./2.jpg";
+import image03 from "./3.jpg";
+import image04 from "./4.jpg";
 import image05 from "./5.jpg";
 import image06 from "./6.jpg";
+import image07 from "./7.jpg";
+import image08 from "./8.jpg";
 import image09 from "./9.jpg";
 import image10 from "./10.jpg";
 import image11 from "./11.jpg";
@@ -11,19 +16,19 @@ import image15 from "./15.jpg";
 import image16 from "./16.jpg";
 export default [
   image01,
+  image02,
+  image03,
+  image04,
   image05,
   image06,
-  image12,
-  image16,
+  image07,
+  image08,
   image09,
   image10,
   image11,
-  image01,
   image12,
-  image06,
   image13,
   image14,
   image15,
-  image05,
   image16,
 ];
